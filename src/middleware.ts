ba3308149c1256397efadc92cc99bd427a0a90b4
g4ze <bhaiyajionline17@gmail.com>
2025-02-21 17:10:15 +0530

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,48 +1,48 @@
 
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-import { verify } from 'jsonwebtoken'
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
+// import { verify } from 'jsonwebtoken'
 
-export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/api/users')) {
-    const authHeader = request.headers.get('authorization')
+// export async function middleware(request: NextRequest) {
+//   if (request.nextUrl.pathname.startsWith('/api/users')) {
+//     const authHeader = request.headers.get('authorization')
     
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      return NextResponse.json(
-        { error: 'Not authenticated' },
-        { status: 401 }
-      )
-    }
+//     if (!authHeader || !authHeader.startsWith('Bearer ')) {
+//       return NextResponse.json(
+//         { error: 'Not authenticated' },
+//         { status: 401 }
+//       )
+//     }
 
-    try {
-      const token = authHeader.split(' ')[1] // Get token from "Bearer <token>"
-      const decoded = verify(token, process.env.JWT_SECRET!) as {
-        userName: string
-        role: string
-      }
+//     try {
+//       const token = authHeader.split(' ')[1] // Get token from "Bearer <token>"
+//       const decoded = verify(token, process.env.JWT_SECRET!) as {
+//         userName: string
+//         role: string
+//       }
 
 
-      if (decoded.userName !== 'admin') {
+//       if (decoded.userName !== 'admin') {
 
-        return NextResponse.json(
-          { error: 'Not authorized - Admin access required' },
-          { status: 403 }
-        )
-      }
+//         return NextResponse.json(
+//           { error: 'Not authorized - Admin access required' },
+//           { status: 403 }
+//         )
+//       }
 
-      return NextResponse.next()
-    } catch (error) {
-      console.error('Error fetching user:', error);
-      return NextResponse.json(
-        { error: 'Invalid token' },
-        { status: 401 }
-      )
-    }
-  }
+//       return NextResponse.next()
+//     } catch (error) {
+//       console.error('Error fetching user:', error);
+//       return NextResponse.json(
+//         { error: 'Invalid token' },
+//         { status: 401 }
+//       )
+//     }
+//   }
 
-  return NextResponse.next()
-}
+//   return NextResponse.next()
+// }
 
-export const config = {
-  matcher: '/api/users/:path*'
-} 
+// export const config = {
+//   matcher: '/api/users/:path*'
+// } 
