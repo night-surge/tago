7b7b14676579cb68cb910610e924589e4bd92a9e
g4ze <bhaiyajionline17@gmail.com>
2025-02-22 02:49:38 +0530

    Revert "Merge branch 'admin-panel' of https://github.com/night-surge/tago into admin-panel"
    This reverts commit bd2c10c32d58c8c1dd41a2b24a8278e5bab20ae3, reversing
    changes made to 98972e854485601b5f9d31406c8d77a858c093b7.
    
    Signed-off-by: g4ze <bhaiyajionline17@gmail.com>

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -64,13 +64,9 @@ export async function POST(req: Request) {
         { status: 401 }
       );
     }
-    let token=null
-    if (user.userName ==='admin'){
-      console.log('admin user logging in, returning token')
-      token=jwt.sign("admin",jwtSecret)
-    } else{
+
     // Create JWT token
-    token = jwt.sign(
+    const token = jwt.sign(
       {
         userId: user.uid,
         userName: user.userName,
@@ -79,7 +75,7 @@ export async function POST(req: Request) {
       },
       jwtSecret
     );
-  }
+
     // Create a safe user object without password
     const safeUser = {
       ...user,
@@ -95,6 +91,9 @@ export async function POST(req: Request) {
       },
       {
         status: 200,
+        headers: {
+          'Set-Cookie': `token=${token}; Path=/; HttpOnly; Max-Age=${7 * 24 * 60 * 60}; SameSite=Lax` // Changed SameSite to Lax
+        }
       }
     );
 
