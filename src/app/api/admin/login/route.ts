347ae8ad66ef1378e58e5d2d7b55990e28bd057a
Nilay Gupta <102874321+g4ze@users.noreply.github.com>
2025-03-04 23:09:11 +0530

    admin_panel (#53)
    * admin_pan
    
    * admin_pan build issues
    
    * build issues brr

---
+++ src/app/api/admin/login/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import jwt from 'jsonwebtoken';
+
+export async function POST(req: Request) {
+  try {
+    const { email, password } = await req.json();
+
+    const admin = await prisma.admin.findUnique({
+      where: { email },
+    });
+    console.log("admin:"+ admin?.email);
+    if (!admin) {
+      return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
+    }
+
+    const passwordMatch = password === admin.password;
+    if (!passwordMatch) {
+      return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
+    }
+
+    const token = jwt.sign(
+      { id: admin.id, email: admin.email },
+      process.env.JWT_SECRET!,
+    );
+
+    return NextResponse.json({ token });
+  } catch (error) {
+    console.error('Login error:', error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+} 
